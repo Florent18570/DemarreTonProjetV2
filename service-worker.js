ea84e24969d70b7be74219022a49476e110f5abe
Florent Escots <Florent.escots@gmail.com>
2022-10-18 23:04:23 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DemarreTonProjetV2/precache-manifest.ca4d527c5fb3a3577ab656267c67895b.js"
+  "/DemarreTonProjetV2/precache-manifest.40991d22dc47ecfda196f08728425e89.js"
 );
 
 workbox.clientsClaim();

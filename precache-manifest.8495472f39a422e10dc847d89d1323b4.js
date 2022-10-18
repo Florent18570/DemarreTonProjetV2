15f962545c3f8440dc2704a57dc728ca934de812
Florent Escots <Florent.escots@gmail.com>
2022-10-18 22:14:44 +0200

    Updates

---
+++ precache-manifest.8495472f39a422e10dc847d89d1323b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da9eda9e14fe1b4bac9a",
+    "url": "/DemarreTonProjetV2/static/css/main.a1d0c7ea.chunk.css"
+  },
+  {
+    "revision": "da9eda9e14fe1b4bac9a",
+    "url": "/DemarreTonProjetV2/static/js/main.da9eda9e.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/DemarreTonProjetV2/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "6b18f6f4c8003acf202d",
+    "url": "/DemarreTonProjetV2/static/js/2.6b18f6f4.chunk.js"
+  },
+  {
+    "revision": "40200a23ee1578aaf9b5",
+    "url": "/DemarreTonProjetV2/static/js/runtime~main.40200a23.js"
+  },
+  {
+    "revision": "b1fe6d6b10d40c44736d9c961364ff9b",
+    "url": "/DemarreTonProjetV2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DemarreTonProjetV2/precache-manifest.9777fde280edba0e02547b144a5e4077.js"
+  "/DemarreTonProjetV2/precache-manifest.8495472f39a422e10dc847d89d1323b4.js"
 );
 
 workbox.clientsClaim();

51798c778cab3b91a03e7dd731bd6ad4e4de13c4
Florent Escots <Florent.escots@gmail.com>
2022-10-18 21:27:01 +0200

    Updates

---
+++ precache-manifest.9176c1175f9fb1a60909e6fbf3ded126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55bad2cf03858ed55372",
+    "url": "/DemarreTonProjetV2/static/css/main.8f7f36c0.chunk.css"
+  },
+  {
+    "revision": "55bad2cf03858ed55372",
+    "url": "/DemarreTonProjetV2/static/js/main.55bad2cf.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/DemarreTonProjetV2/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/DemarreTonProjetV2/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "def0a29778d4cfcec55d",
+    "url": "/DemarreTonProjetV2/static/js/runtime~main.def0a297.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/DemarreTonProjetV2/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2a46e26957b27977399f216e5816b4b8",
+    "url": "/DemarreTonProjetV2/index.html"
+  }
+];

b78d6f92d716ddf90952e300dd5c3f921b65c4cd
Florent Escots <Florent.escots@gmail.com>
2022-11-01 14:14:27 +0100

    header vPC

--- src/components/containers/header/Header.jsx
+++ src/components/containers/header/Header.jsx
@@ -3,31 +3,32 @@ import logo from "../../../Images/Logo.png";
 
 const Header = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__links">
-        <img className="navbar__links__logo" src={logo} alt="logo" />
-        <a className="navbar__links__a" href="#Accueil">
-          Accueil
-        </a>
-        <a className="navbar__links__a" href="#Prestations">
-          <p>Prestations</p>
-        </a>
-        <a className="navbar__links__a " href="#Réalisations">
-          <p>Réalisations</p>
-        </a>
-        <a className="navbar__links__a devis" href="#Tarifs">
-          <p>Faire un devis</p>
-        </a>
-        <a className="navbar__links__a " href="#L'agence">
-          <p>L'agence</p>
-        </a>
-      </div>
-      <div className="navbar__links__end">
-        <a href="#contact">
-          <p>Contact </p>
-        </a>
-      </div>
-    </div>
+    <header className="navbar">
+      <img className="navbar__logo" src={logo} alt="logo" />
+
+      <nav>
+        <ul className="navbar__links">
+          <li>
+            <a href="#Accueil">Accueil</a>
+          </li>
+          <li>
+            <a href="#Prestations"> Prestations</a>
+          </li>
+          <li>
+            <a href="#Réalisations"> Réalisations</a>
+          </li>
+          <li>
+            <a href="#Devis"> Faire un devis</a>
+          </li>
+          <li>
+            <a href="#L'agence"> L'agence</a>
+          </li>
+        </ul>
+      </nav>
+      <a class="cta" href="#Contact">
+        <button>Contact</button>
+      </a>
+    </header>
   );
 };
 

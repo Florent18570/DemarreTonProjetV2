a0431b23e39ebba36883bc1cd960432ae804cba2
Florent Escots <Florent.escots@gmail.com>
2022-11-01 14:14:54 +0100

    Updates

---
+++ precache-manifest.71ccd746d61ec6904fd3354a49cab9e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a54eecdcce55b6675389",
+    "url": "/DemarreTonProjetV2/static/css/main.017bb1c5.chunk.css"
+  },
+  {
+    "revision": "a54eecdcce55b6675389",
+    "url": "/DemarreTonProjetV2/static/js/main.a54eecdc.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/DemarreTonProjetV2/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "66dc41ccecf657aa0e0d",
+    "url": "/DemarreTonProjetV2/static/js/2.66dc41cc.chunk.js"
+  },
+  {
+    "revision": "4099b6956cc47f4cfab1",
+    "url": "/DemarreTonProjetV2/static/js/runtime~main.4099b695.js"
+  },
+  {
+    "revision": "cf5f71f0a3a4f28a7bf8e2c2e29cf79e",
+    "url": "/DemarreTonProjetV2/static/media/Logo.cf5f71f0.png"
+  },
+  {
+    "revision": "177174ad33e11796d361fd7768f7db6f",
+    "url": "/DemarreTonProjetV2/static/media/refonte.177174ad.png"
+  },
+  {
+    "revision": "1809256e5a2cc83ad71d5ead384a7812",
+    "url": "/DemarreTonProjetV2/index.html"
+  }
+];

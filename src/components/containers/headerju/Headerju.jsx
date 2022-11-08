65ec8a02ac921fe889275b1e7bb6a50286b61de7
Florent Escots <Florent.escots@gmail.com>
2022-11-08 16:27:40 +0100

    nous

--- src/components/containers/headerju/Headerju.jsx
+++ src/components/containers/headerju/Headerju.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 const HeaderJu = () => {
-  return <div className="Header_global" >
-    <p className="Header_titre">DemarreTonProjet</p>
-    <h1 className="Header_creation">Création de sites internet</h1>
-    <p className="Header_description">Avoir un site performant n'a jamais était si facile. Rapide, adapté et optimisé, votre site répondra à toutes vos attentes.</p>
-    <button className="Header_button"> Me contacter </button>
+  return (
+    <div className="Header_global">
+      <p className="Header_titre">DemarreTonProjet</p>
+      <h1 className="Header_creation">Création de sites internet</h1>
+      <p className="Header_description">
+        Avoir un site performant n'a jamais était si facile. Rapide, adapté et
+        optimisé, votre site répondra à toutes vos attentes.
+      </p>
+      <button className="Header_button"> Me contacter </button>
     </div>
+  );
 };
 
 export default HeaderJu;

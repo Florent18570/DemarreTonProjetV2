17e8ce50a7de02d6dfc847e3aa9f5198567ea3de
OpixO <32876856+OpixO@users.noreply.github.com>
2022-11-10 16:21:00 +0100

    devis button

--- src/components/containers/devis/Devis.jsx
+++ src/components/containers/devis/Devis.jsx
@@ -1,40 +1,112 @@
 import React from "react";
+import grid1 from "../../../Images/grid.png";
+import grid2 from "../../../Images/grid2.png";
+import grid3 from "../../../Images/grid3.png";
+import grid4 from "../../../Images/grid4.png";
 
 
 const Devis = () => {
 
-  function etape2(){
-    document.getElementById("num").style.visibility = "collapse";
-    document.getElementById("num2").style.visibility = "visible";
+ 
 
-  }
-  return <div className="devis ">
-    <div className="sectionbox">
-      <div className="Devis_bloc" id="num">
-        <h3>Etape 1</h3>
-        <input type="text"></input>
-        <div className="Devis_Bloc_choix">
+  var switchdevis = 0;
 
-          <button onClick={etape2}>Retour</button>
-          <button>Suivant</button>
-        </div>
-      
-      
-      </div>
-
-      <div className="Devis_bloc_2" id="num2">
-        <h3>Etape 2</h3>
-        <input type="text"></input>
-        <div className="Devis_Bloc_choix">
-
-          <button>Retour</button>
-          <button>Suivant</button>
-        </div>
-      
-      
-      </div>
+  function devischange(choix) {
+    switchdevis = choix;
+    console.log(switchdevis);
     
-    </div>
+    var buttonoui = document.getElementsByName("ouiidee")[0];
+    var buttonnon = document.getElementsByName("nonidee")[0];
+
+    if (switchdevis == 1) {
+      //menu désactivé
+      buttonoui.classList.add("animation_choixdevis");
+      buttonoui.classList.remove("animation_choixdevis_reverse");
+
+      buttonnon.classList.remove("animation_choixdevis");
+      buttonnon.classList.add("animation_choixdevis_reverse");
+
+    } else {
+      //menu activé
+      buttonoui.classList.remove("animation_choixdevis");
+      buttonoui.classList.add("animation_choixdevis_reverse");
+
+      buttonnon.classList.add("animation_choixdevis");
+      buttonnon.classList.remove("animation_choixdevis_reverse");
+
+      
+    }
+  }
+
+  return <div>
+    <section className="Prestation">
+      <h2> Faites vous accompagner pour votre visibilité sur internet </h2>
+
+      
+      <div className="Prestation__grid">
+
+        <button onClick={() => devischange(1)} >
+        <div className="Prestation__grid__card">
+          <div className="Prestation__grid__card__img">
+            <img src={grid1} alt="grid1" />
+          </div>
+          <div className="Prestation__grid__card__content">
+            <h3> J'ai une idée de site</h3>
+            <p>
+              Afin de concevoir un site au plus proche de votre souhait, nous vous laissons remplir le formulaire de contact afin que nous puissions répondre à votre demande au plus vite.
+            </p>
+            <a href=""> En savoir plus</a>
+          </div>
+        </div>
+        </button>
+
+        <button onClick={() => devischange(2)}>
+        <div className="Prestation__grid__card">
+          <div className="Prestation__grid__card__img">
+            <img src={grid2} alt="grid1" />
+          </div>
+          <div className="Prestation__grid__card__content">
+            <h3>Je n'ai aucune idée du visuel</h3>
+            <p>
+              Le design ? Nous pouvons nous en occuper. Pour nous aider et vous donner rapidement et en ligne une fourchette de prix, nous vous laissons remplir le formulaire suivant.
+            </p>
+            <a href=""> En savoir plus</a>
+          </div>
+        </div>
+        </button>
+
+        
+      </div>
+
+      <div className="Prestation__grid__form">
+
+      <div name="ouiidee" className="Prestation__grid__choix">
+          <div className="Prestation__grid__card__img">
+            <img src={grid2} alt="grid1" />
+          </div>
+          <div className="Prestation__grid__card__content">
+            <h3>Jezjnzekjnezkjzeezkne n'ai aucune idée du visuel</h3>
+            <p>
+              Le design ? Nous pouvons nous en occuper. Pour nous aider et vous donner rapidement et en ligne une fourchette de prix, nous vous laissons remplir le formulaire suivant.
+            </p>
+            <a href=""> En savoir plus</a>
+          </div>
+        </div>
+
+        <div name="nonidee" className="Prestation__grid__choix">
+          <div className="Prestation__grid__card__img">
+            <img src={grid2} alt="grid1" />
+          </div>
+          <div className="Prestation__grid__card__content">
+            <h3>Je n'ai aucune idée du visuel</h3>
+            <p>
+              Le design ? Nous pouvons nous en occuper. Pour nous aider et vous donner rapidement et en ligne une fourchette de prix, nous vous laissons remplir le formulaire suivant.
+            </p>
+            <a href=""> En savoir plus</a>
+          </div>
+        </div>
+      </div>
+    </section>
     </div>
 };
 

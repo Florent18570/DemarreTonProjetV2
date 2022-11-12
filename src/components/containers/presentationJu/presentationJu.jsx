e3d24a66b755a9133aa30b704aee9419c0c98d1e
Florent Escots <Florent.escots@gmail.com>
2022-11-12 15:32:18 +0100

    erreur marge

--- src/components/containers/presentationJu/presentationJu.jsx
+++ src/components/containers/presentationJu/presentationJu.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { tns } from "../../../../node_modules/tiny-slider/src/tiny-slider";
 import test from "../../../Images/test.png";
-
+import stockDataa from "../../../data/card";
 var deplacement = [100, 200, 300];
 var dep = 0;
 
@@ -33,33 +34,71 @@ const PresentationJu = () => {
     }
   }
 
-  return (
-    <div className="Presentation_glob">
-      <div className="Presentation">
-        {stockData.map((data, key) => {
-          return (
-            <div key={key} className="Presentation__card">
-              <img src={test} alt="test" />
+  useEffect(() => {
+    tns({
+      container: ".my-slider",
+      slideBy: 1,
+      speed: 400,
+      nav: false,
+      rewind: true,
+      // swipeAngle: false,
+      prevButton: ".previous",
+      nextButton: ".next",
+      responsive: {
+        1600: {
+          items: 3,
+        },
+        1024: {
+          items: 3,
+        },
+        768: {
+          items: 2,
+        },
+        480: {
+          items: 1,
+        },
+      },
+    });
+  }, []);
 
-              <div className="Presentation__card__content">
-                <p className="Presentation__card__content__categorie">
-                  {data.service}
-                </p>
-                <p className="Presentation__card__content__title">
-                  {data.compagnie}
-                </p>
+  return (
+    <>
+      <div className="Presentation__glob">
+        <div className="Presentation__left">
+          <h3> Les derniers sites</h3>
+          <p> Toujours pas convaincu ? Regarder mes réalisations.</p>
+          <button> Voir toutes mes réalisations</button>
+        </div>
+        <div className="Presentation__right my-slider">
+          {stockData.map((data, key) => {
+            return (
+              <div className="slide">
+                <div key={key} className="Presentation__right__card ">
+                  <img src={test} alt="test" />
+
+                  <div className="Presentation__right__card__content">
+                    <p className="Presentation__right__card__content__categorie">
+                      {data.service}
+                    </p>
+                    <p className="Presentation__right__card__content__title">
+                      {data.compagnie}
+                    </p>
+                  </div>
+                </div>{" "}
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
         <div>
           {/* <FontAwesomeIcon icon={faCoffee} />
           <FontAwesomeIcon icon={faarrowright} /> */}
-          <button onClick={moveUp}>Avant</button>
-          <button onClick={moveDown}>Après</button>
         </div>
       </div>
-    </div>
+      <div className="controlebutton">
+        <button className="previous">Avant</button>
+        <button className="next">Après</button>
+      </div>
+    </>
   );
 };
 

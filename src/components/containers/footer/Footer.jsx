9538daaad81f0ad8123ab18185bafa16c05ae417
Florent Escots <Florent.escots@gmail.com>
2022-11-23 11:02:41 +0100

    footer

--- src/components/containers/footer/Footer.jsx
+++ src/components/containers/footer/Footer.jsx
@@ -2,13 +2,40 @@ import React from "react";
 import { FaBeer } from "react-icons/fa";
 import card_footer from "../../../Images/card_footer.png";
 
+import instagram from "../../../Images/instagram.png";
+import twitter from "../../../Images/twitter.png";
+import facebook from "../../../Images/facebook.png";
+
 const Footer = () => {
   return (
     <>
       <div className="Footer">
         <div className="Footer__left">
           <img src={card_footer} alt="card_footer" />
+          <div className="info__footer">
+            <div className="info__footer__div p1">
+              <h3> Téléphone </h3>
+              <p> 07 62 06 88 30</p>
+            </div>
+            <div className="info__footer__div__border"></div>
+            <div className="info__footer__div p2">
+              <h3> Adresse mail </h3>
+              <p> DemarreTonProjet@gmail.com</p>
+            </div>
+            <div className="info__footer__div__border p3"></div>
+            <div className="info__footer__div p3">
+              <img src={instagram} alt="instagram logo" />
+              <img src={twitter} alt="twitter" />
+              <img src={facebook} alt="facebook" />
+            </div>
+          </div>
         </div>
+        <div className="reseau">
+          <img src={instagram} alt="instagram logo" />
+          <img src={twitter} alt="twitter" />
+          <img src={facebook} alt="facebook" />
+        </div>
+
         <div className="Footer__right">
           <div className="Footer__right__devis">
             <p> Estimons le cout de votre projet </p>

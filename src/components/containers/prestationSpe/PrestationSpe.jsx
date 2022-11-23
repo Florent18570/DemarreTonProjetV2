55fb911fb41ba8107c7b1e996196096bdae5ee52
OpixO <32876856+OpixO@users.noreply.github.com>
2022-11-24 00:24:56 +0100

    start

--- src/components/containers/prestationSpe/PrestationSpe.jsx
+++ src/components/containers/prestationSpe/PrestationSpe.jsx
@@ -14,9 +14,38 @@ const PrestationSpe = () => {
 
     if(params.prestation == "creation"){
         return (
-            <section className="Prestation">
-              <h2 className="Prestationspe_titre">Création de votre site internet</h2>
-            </section>
+            <div>
+                <section className="Prestation">
+                    <h2 className="Prestationspe_titre">Création de votre site internet</h2>
+                </section>
+                <div className="Prestationspe_presentation">
+                    <div className="Prestationspe_Presentation_text">
+                        <h3 className="Prestationspe_Presentation_text_titre">Notre équipe, notre expertise</h3>
+                        <p className="Prestationspe_Presentation_text_titre">bla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla blbla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blab </p>
+                    </div>
+                    <div className="Prestationspe_Presentation_Image">
+                        <img src={grid4}></img>
+                    </div>
+                    
+
+                </div>
+                <div className="Prestationspe_separation">
+                    <h2 className="Prestationspe_Separation_titre">Réaliser votre site web à nos côtés</h2>
+                    <button className="Prestationspe_Separation_button">Je réalise un devis</button>
+                </div>
+                <div className="Prestationspe_explication">
+                    <h3 className="Prestationspe_Explication_accroche">Une méthode simple et efficace</h3>
+                    <h2 className="Prestationspe_Explication_titre">Commercialiser vos produits ou services en ligne</h2>
+                    <div className="Prestationspe_Explication_detail">
+                        <div className="Prestationspe_Explication_Detail_text">
+                            <h3>Mettez toutes les chances de votre côtés pour convertir un client</h3>
+                            <p>bla bla bla bla faudra que je réflechisse à mettre un texte intéressant</p>
+                            <button>Demander un avis d'expert</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
           );
     }else if( params.prestation == "uxdesign"){
         return (

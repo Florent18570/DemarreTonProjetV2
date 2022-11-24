d57740f08ff9c619195521b7331d6cdc42ca1871
Florent Escots <Florent.escots@gmail.com>
2022-11-24 15:40:06 +0100

    correction marge

--- src/components/containers/presentationJu/presentationJu.jsx
+++ src/components/containers/presentationJu/presentationJu.jsx
@@ -1,15 +1,11 @@
-import React, {  useEffect } from "react";
-import {  Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { tns } from "../../../../node_modules/tiny-slider/src/tiny-slider";
 import test from "../../../Images/test.png";
 import stockDataa from "../../../data/card";
 
-
 const PresentationJu = () => {
   var stockData = stockDataa();
-
-
-  
 
   useEffect(() => {
     tns({
@@ -25,11 +21,11 @@ const PresentationJu = () => {
         1600: {
           items: 3,
         },
-        1180: {
+        1023: {
           items: 2,
         },
         768: {
-          items: 1,
+          items: 2,
         },
         640: {
           items: 2,
@@ -44,47 +40,53 @@ const PresentationJu = () => {
         <div className="Presentation__left">
           <h2 className="Presentation__H2"> Les derniers sites</h2>
           <div className="Presentation__left__text">
-          <p className="Presentation__left__text__p"> Toujours pas convaincu ? Vous pouvez regarder nos créations publics sur la partie d'à côté. Des boutons sont disponibles pour vous permettre de naviguer entre eux. Regarder mes réalisations.</p>
-          <Link to="/allrealisation">
-          <button className="Presentation__left__text__button"> Voir toutes mes réalisations</button>
-          </Link>
-          
+            <p className="Presentation__left__text__p">
+              Toujours pas convaincu ? Vous pouvez regarder nos créations
+              publics sur la partie d'à côté. Des boutons sont disponibles pour
+              vous permettre de naviguer entre eux. Regarder mes réalisations.
+            </p>
+
+            <button
+              href="/allrealisation"
+              className="Presentation__left__text__button"
+            >
+              Voir toutes mes réalisations
+            </button>
           </div>
         </div>
         <div className="Presentation_card_bg">
-        <div className="Presentation__right my-slider">
-          {stockData.map((data, key) => {
-            return (
-              <div className="slide">
-                <a href={"/realisation/"+data.id}>
-                <div key={key} className="Presentation__right__card ">
-                  <img src={data.src} alt="test" />
+          <div className="Presentation__right my-slider">
+            {stockData.map((data, key) => {
+              return (
+                <div className="slide">
+                  <a href={"/realisation/" + data.id}>
+                    <div key={key} className="Presentation__right__card ">
+                      <img src={data.src} alt="test" />
 
-                  <div className="Presentation__right__card__content">
-                    <p className="Presentation__right__card__content__categorie">
-                      {data.service}
-                    </p>
-                    <p className="Presentation__right__card__content__title">
-                      {data.compagnie}
-                    </p>
-                  </div>
-                </div>{" "}
-                </a>
-              </div>
-            );
-          })}
-        </div>
-        <div className="sfd">
-        <button className="previous"> &#8249; &#8249; </button>
-        <button className="next"> &#8250; &#8250; </button>
-      </div>
+                      <div className="Presentation__right__card__content">
+                        <p className="Presentation__right__card__content__categorie">
+                          {data.service}
+                        </p>
+                        <p className="Presentation__right__card__content__title">
+                          {data.compagnie}
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+          <div className="sfd">
+            <button className="previous"> &#8249; &#8249; </button>
+            <button className="next"> &#8250; &#8250; </button>
+          </div>
         </div>
         <div>
           {/* <FontAwesomeIcon icon={faCoffee} />
           <FontAwesomeIcon icon={faarrowright} /> */}
         </div>
       </div>
-      
     </>
   );
 };

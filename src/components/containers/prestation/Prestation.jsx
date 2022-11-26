ad932aa3a146a1a22b86c04bbea37f7b6851bc13
Florent Escots <Florent.escots@gmail.com>
2022-11-26 17:05:47 +0100

    correction prestation

--- src/components/containers/prestation/Prestation.jsx
+++ src/components/containers/prestation/Prestation.jsx
@@ -20,7 +20,7 @@ const Prestation = () => {
               Développement de votre site sur-mesure avec toutes les
               fonctionnalit"s nécessaires.
             </p>
-            <a href="/creation"> En savoir plus</a>
+            <a href="/prestation/creation"> En savoir plus</a>
           </div>
         </div>
 
@@ -34,7 +34,7 @@ const Prestation = () => {
               Optimisation de l'expérience utilisateur pour améliorer la
               conversation.
             </p>
-            <a href="/uxdesign"> En savoir plus</a>
+            <a href="/prestation/uxdesign"> En savoir plus</a>
           </div>
         </div>
 
@@ -48,7 +48,7 @@ const Prestation = () => {
               Amélioration de votre position dans les moteurs de recherche avec
               le trafic organique.
             </p>
-            <a href="/referencement"> En savoir plus</a>
+            <a href="/prestation/referencement"> En savoir plus</a>
           </div>
         </div>
 
@@ -62,7 +62,7 @@ const Prestation = () => {
               Hébergement, cybersécurité, sauvegarde, maintenance de votre
               application
             </p>
-            <a href="/maintenance"> En savoir plus</a>
+            <a href="/prestation/maintenance"> En savoir plus</a>
           </div>
         </div>
       </div>

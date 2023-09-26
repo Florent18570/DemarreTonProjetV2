1c12575628f26fce2649dce1914f7a8798dea969
OpixO <32876856+OpixO@users.noreply.github.com>
2023-09-26 06:05:04 +0200

    Revert "Revert "Revert "fail"""
    This reverts commit 1460104a6f97caf0bc5c81480d15f7b51de70f60.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./styles/main.css";
 import "./styles/normalise.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import NotFound from "./pages/notfound";
+import Devis from "./pages/devis";
+import Realisation from "./pages/realisation";
+import Allrealisation from "./pages/allrealisation";
+import PrestationSpe from "./pages/prestationspe";
 
 export default function App() {
   return (
     <Router>
-      <Home exact path="/" />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/devis" element={<Devis />} />
+        <Route exact path="/allrealisation" element={<Allrealisation />} />
+        <Route exact path="/realisation/:userId" element={<Realisation />} />
+        <Route
+          exact
+          path="/prestation/:prestation"
+          element={<PrestationSpe />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }

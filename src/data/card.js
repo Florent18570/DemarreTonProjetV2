93c4e2862142726bbdcae57b1c99e10136d9124d
OpixO <32876856+OpixO@users.noreply.github.com>
2023-10-11 14:35:42 +0200

    add du site de thilane

--- src/data/card.js
+++ src/data/card.js
@@ -210,6 +210,44 @@ export default function stockDataa() {
         },
       ],
     },
+    {
+      id: "5",
+      compagnie: "aba-montpellier",
+      intro: "Développé",
+      service: "Site Vitrine",
+      technologie: "Wordpress",
+      date: "Septemblre 2023",
+      site: "https://aba-montpellier.fr",
+      link: "https://aba-montpellier.fr",
+      src: "/images/abamontpellier.webp",
+      processus: [
+        {
+          etape: "1. Rédaction d'une charte graphique",
+          description:
+            "Comme tout projet, une charte graphique est au préalable développée afin que le site corresponde au mieux à l'identité visuelle de l'entreprise.",
+        },
+        {
+          etape: "2. Design des wireframes et de la maquette",
+          description:
+            "En reprenant la charte graphique, la totalité des pages a d'abord été wireframée, puis, sur Figma, le design a été fait. Après validation du client, nous avons pu passer à la suite.",
+        },
+        {
+          etape: "3. Choix de la solution",
+          description:
+            "La solution devait s'intégrer dans l'écosystème déjà existant, j'ai décidé d'utiliser Webflow pour ensuite exporter le code en HTML/CSS/JS et l'intégrer à la solution.",
+        },
+        {
+          etape: "4. Développement du site",
+          description:
+            " Une fois la maquette validée et la solution choisie, j'ai pu passer au développement du site.",
+        },
+        {
+          etape: "5. Test de performance PageSpeed Insights de Google",
+          description:
+            "Tous mes sites sont optimisés afin d'être le plus performant possible.",
+        },
+      ],
+    },
   ];
 
   return stockData;
